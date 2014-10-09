bc96dcd7c4704af2326c8b4a05904e0b62912072
Nastase Alexandru <admin@movieclip.ro>
2014-10-09 18:48:16 +0300

    initial commit

---
+++ src/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+   
+   'api'           => 'v1',
+
+   'appid'         => 'iphone1_1',
+
+   'apiPolicy'     => 'app1_1',
+
+   'apiKey'        => '2wex6aeu6a8q9e49k7sfvufd6rhh0n',
+   
+   'locale'        =>  'en_US',
+   
+   'timestamp'     => '',
+   
+   'anonymiser'    => 'http://anonymouse.org/cgi-bin/anon-www.cgi/',
+   
+   'anonymise'     => false
+);
